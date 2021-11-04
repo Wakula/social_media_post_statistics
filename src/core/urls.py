0cfd353b15dc2b5c1828bad5dd6e7b6a16dc79ae
Kiril Vakulenko <wither04@gmail.com>
2021-11-04 14:16:51 +0200

    Remove redundant "posts_service" substring in base url name.

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path('api/statistics_service/', include('post_statistics.urls')),
+    path('api/', include('post_statistics.urls')),
 ]
